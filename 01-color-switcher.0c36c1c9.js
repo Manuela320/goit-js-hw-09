473cee9ed2813b36273ca301a72d78afb503e68b
Manuela320 <157847678+Manuela320@users.noreply.github.com>
2024-07-12 15:35:53 +0000

    Deploying to gh-pages from @ Manuela320/goit-js-hw-09@2469cf6a4e28de22f25f93a42ec7c8492d916116 🚀

---
+++ 01-color-switcher.0c36c1c9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{t.disabled=!0,a=setInterval(d,1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.0c36c1c9.js.map
